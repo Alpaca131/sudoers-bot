73e92c9c38da343fa9ee83142d90d894c04fdab8
Alpaca131 <iwa124816+github@gmail.com>
2022-12-04 15:50:29 +0000

    store sudo_users

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 from discord import app_commands
-import asyncio
+import asyncio, json, time
 
 import envs
+
+file_loaded = False
+sudo_users = {}
 
 
 class MyClient(discord.Client):
@@ -27,6 +30,26 @@ class MyClient(discord.Client):
 client = MyClient(intents=discord.Intents.all())
 
 
+@client.event
+async def on_ready():
+    global file_loaded
+    print("logged in")
+    if file_loaded:
+        return
+    with open("sudo_users.json", "r") as f:
+        stored_sudo_users = json.load(f)
+        sudo_users.update(stored_sudo_users)
+    async_tasks = []
+    for guild_id in stored_sudo_users:
+        for user_id in stored_sudo_users[guild_id]:
+            expiry_time = stored_sudo_users[guild_id][user_id]
+            async_tasks.append(asyncio.create_task(await_sudo_expiry(expiry_time, guild_id, user_id)))
+    if async_tasks:
+        await asyncio.wait(async_tasks)
+        write_sudo_users()
+    file_loaded = True
+
+
 # Define the "sudo" command
 @client.tree.command()
 async def sudo(interaction: discord.Interaction):
@@ -35,11 +58,36 @@ async def sudo(interaction: discord.Interaction):
         # Give the user the "sudo" role
         await interaction.user.add_roles(discord.utils.get(interaction.guild.roles, name="sudo"))
         await interaction.response.send_message("You have been granted sudo access. Valid for 3 min.", ephemeral=True)
-        await asyncio.sleep(3*60)
+        add_sudo_users(interaction.guild.id, interaction.user.id)
+        await asyncio.sleep(3 * 60)
         await interaction.user.remove_roles(discord.utils.get(interaction.guild.roles, name="sudo"))
     else:
         # The user does not have the "sudoers" role, so do nothing
         await interaction.response.send_message("You do not have the sudoers role!")
+
+
+def add_sudo_users(guild_id, user_id):
+    if guild_id not in sudo_users:
+        sudo_users[guild_id] = {}
+    # Record expiry time
+    sudo_users[guild_id][user_id] = time.time() + 60*3
+    write_sudo_users()
+
+
+def write_sudo_users():
+    with open("sudo_users.json", "w") as f:
+        json.dump(sudo_users, f)
+
+
+async def await_sudo_expiry(expiry: int, guild_id: int, user_id: int):
+    seconds_to_wait = expiry - time.time()
+    if seconds_to_wait > 0:
+        await asyncio.sleep(seconds_to_wait)
+    guild = client.get_guild(int(guild_id))
+    member = guild.get_member(int(user_id))
+    await member.remove_roles(discord.utils.get(guild.roles, name="sudo"))
+    sudo_users[guild_id].pop(user_id)
+    print(f"Removed sudo from {member}")
 
 # Run the bot
 client.run(envs.TOKEN)
